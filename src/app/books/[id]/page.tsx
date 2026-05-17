b1982e2d35f713237fa164885b1025b240008b80
LassazVegaz <lasindunuwanga@gmail.com>
2026-05-17 19:15:22 +0800

    create book basic UI

---
+++ src/app/books/[id]/page.tsx
@@ -0,0 +1,33 @@
+import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+
+const FieldsContainer = ({ className, ...props }: ComponentProps<"div">) => (
+  <div className={twMerge("flex items-center", className)} {...props} />
+);
+
+const Label = ({ className, ...props }: ComponentProps<"label">) => (
+  <label className={twMerge("w-37.5", className)} {...props} /> // NOSONAR
+);
+
+const InputField = ({ className, ...props }: ComponentProps<"input">) => (
+  <input className={twMerge("border p-2 rounded", className)} {...props} />
+);
+
+export default function BookPage() {
+  return (
+    <div className="h-full w-full flex justify-center items-center">
+      <form className="border p-4 rounded">
+        <FieldsContainer>
+          <Label htmlFor="title">Title</Label>
+          <InputField type="text" name="title" />
+        </FieldsContainer>
+
+        <div className="flex justify-center mt-10">
+          <button type="submit" className="bg-blue-500 px-4 py-2 rounded">
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}

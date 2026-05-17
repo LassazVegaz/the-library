0363cfcbbde47b06c9a0ce07c13e0a72c6484707
LassazVegaz <lasindunuwanga@gmail.com>
2026-05-17 19:56:52 +0800

    create book link

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import PageTitle from "@/components/PageTitle";
+import FloatingActionButton from "@/components/FloatingActionButton";
 
 const bookTitles = [
   "The Great Gatsby",
@@ -18,21 +19,25 @@ const bookTitles = [
 
 export default function BooksPage() {
   return (
-    <div className="py-page-gutter grid grid-rows-[auto_1fr] max-h-full">
-      <PageTitle>Books</PageTitle>
+    <>
+      <div className="py-page-gutter grid grid-rows-[auto_1fr] max-h-full">
+        <PageTitle>Books</PageTitle>
 
-      <ul className="mt-10 max-w-md w-full mx-auto flex flex-col gap-4 overflow-y-auto px-5 styled-scrollbar">
-        {bookTitles.map((title) => (
-          <li key={title}>
-            <Link
-              href="books/1"
-              className="block border border-gray-300 rounded-md p-2 hover:bg-gray-100 hover:text-black duration-500 cursor-pointer"
-            >
-              {title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+        <ul className="mt-10 max-w-md w-full mx-auto flex flex-col gap-4 overflow-y-auto px-5 styled-scrollbar">
+          {bookTitles.map((title) => (
+            <li key={title}>
+              <Link
+                href="books/1"
+                className="block border border-gray-300 rounded-md p-2 hover:bg-gray-100 hover:text-black duration-500 cursor-pointer"
+              >
+                {title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <FloatingActionButton href="books/new">+</FloatingActionButton>
+    </>
   );
 }

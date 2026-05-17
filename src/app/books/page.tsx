9f09e672227c0f8d5577317db432d1574ae3409c
LassazVegaz <lasindunuwanga@gmail.com>
2026-05-17 19:50:56 +0800

    books to book pages link

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import PageTitle from "@/components/PageTitle";
 
 const bookTitles = [
@@ -20,13 +21,15 @@ export default function BooksPage() {
     <div className="py-page-gutter grid grid-rows-[auto_1fr] max-h-full">
       <PageTitle>Books</PageTitle>
 
-      <ul className="mt-10 max-w-md w-full mx-auto flex flex-col gap-2 overflow-y-auto px-5 styled-scrollbar">
+      <ul className="mt-10 max-w-md w-full mx-auto flex flex-col gap-4 overflow-y-auto px-5 styled-scrollbar">
         {bookTitles.map((title) => (
-          <li
-            className="border border-gray-300 rounded-md p-2 mb-2 hover:bg-gray-100 hover:text-black duration-500 cursor-pointer"
-            key={title}
-          >
-            {title}
+          <li key={title}>
+            <Link
+              href="books/1"
+              className="block border border-gray-300 rounded-md p-2 hover:bg-gray-100 hover:text-black duration-500 cursor-pointer"
+            >
+              {title}
+            </Link>
           </li>
         ))}
       </ul>

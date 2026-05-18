4662711c0d9ef6930825c4b7d809ff8d92510ead
LassazVegaz <lasindunuwanga@gmail.com>
2026-05-18 20:59:57 +0800

    list books

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,36 +1,24 @@
 import Link from "next/link";
 import PageTitle from "@/components/PageTitle";
 import FloatingActionButton from "@/components/FloatingActionButton";
+import booksService from "@/services/books.service";
 
-const bookTitles = [
-  "The Great Gatsby",
-  "To Kill a Mockingbird",
-  "1984",
-  "Pride and Prejudice",
-  "The Catcher in the Rye",
-  "The Lord of the Rings",
-  "Harry Potter and the Sorcerer's Stone",
-  "The Hobbit",
-  "The Hobbit 2",
-  "The Hobbit 3",
-  "The Hobbit 4",
-  "The Hobbit 5",
-];
+export default async function BooksPage() {
+  const books = await booksService.getAll();
 
-export default function BooksPage() {
   return (
     <>
       <div className="py-page-gutter grid grid-rows-[auto_1fr] max-h-full">
         <PageTitle>Books</PageTitle>
 
         <ul className="mt-10 max-w-md w-full mx-auto flex flex-col gap-4 overflow-y-auto px-5 styled-scrollbar">
-          {bookTitles.map((title) => (
-            <li key={title}>
+          {books.map((b) => (
+            <li key={b.id}>
               <Link
-                href="books/1"
+                href={`books/${b.id}`}
                 className="block border border-gray-300 rounded-md p-2 hover:bg-gray-100 hover:text-black duration-500 cursor-pointer"
               >
-                {title}
+                {b.title}
               </Link>
             </li>
           ))}

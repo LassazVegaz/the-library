e3a120ebd5026691e6152046c8feee1f8777703d
LassazVegaz <lasindunuwanga@gmail.com>
2026-05-18 16:34:38 +0800

    simple books service

---
+++ src/services/books.service.ts
@@ -0,0 +1,30 @@
+import { Book } from "@/generated/prisma/client";
+import prisma from "@/lib/prisma";
+
+type UpdateBook = Partial<Pick<Book, "title">>;
+
+class BooksService {
+  async create(book: Book) {
+    return await prisma.book.create({ data: book });
+  }
+
+  async update(id: number, book: UpdateBook) {
+    return await prisma.book.update({ where: { id }, data: book });
+  }
+
+  async delete(id: number) {
+    await prisma.book.delete({ where: { id } });
+  }
+
+  async findById(id: number) {
+    return await prisma.book.findUnique({ where: { id } });
+  }
+
+  async getAll() {
+    return await prisma.book.findMany();
+  }
+}
+
+const booksService = new BooksService();
+
+export default booksService;

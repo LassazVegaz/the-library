08d81987b55efac4a2a117ef4192cee00f7f49be
LassazVegaz <lasindunuwanga@gmail.com>
2026-05-19 15:38:16 +0800

    error handlers lib

---
+++ src/lib/client/error-handlers.ts
@@ -0,0 +1,24 @@
+export const handleServerError = (error: unknown) => {
+  if (
+    typeof error === "object" &&
+    error !== null &&
+    "message" in error &&
+    error.message === "NEXT_REDIRECT"
+  ) {
+    throw error; // Rethrow the redirect error to be handled by Next.js
+  } else {
+    console.error("An error occurred while submitting the form:", error);
+    alert("An error occurred while submitting the form. Please try again.");
+  }
+};
+
+export const handleActionErrors = (
+  error: { formErrors: Record<string, string[]> } | { serverError: true },
+) => {
+  if ("serverError" in error) {
+    alert("An error occurred while submitting the form. Please try again.");
+  } else {
+    const firstMessage = Object.values(error.formErrors)[0]?.[0];
+    if (firstMessage) alert(firstMessage);
+  }
+};

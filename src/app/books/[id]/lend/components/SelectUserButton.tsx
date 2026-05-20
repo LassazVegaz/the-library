3120c282e0fffe9fd43c923676e0f12df0f57ac0
LassazVegaz <lasindunuwanga@gmail.com>
2026-05-20 17:15:14 +0800

    navigate to selected user

--- src/app/books/[id]/lend/components/SelectUserButton.tsx
+++ src/app/books/[id]/lend/components/SelectUserButton.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { Button } from "@/components/FormComponents";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 type SelectUserButtonProps = {
   dialogId: string;
@@ -8,12 +10,45 @@ type SelectUserButtonProps = {
 export default function SelectUserButton(
   props: Readonly<SelectUserButtonProps>,
 ) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
   const onClick = () => {
     const dialogEl = document.getElementById(
       props.dialogId,
     ) as HTMLDialogElement | null;
     dialogEl?.showModal();
   };
+
+  // react compiler handles useCallback
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const onDialogClose = () => {
+    const dialogEl = document.getElementById(
+      props.dialogId,
+    ) as HTMLDialogElement | null;
+
+    const userId = dialogEl?.returnValue;
+    if (!userId) return;
+
+    const params = new URLSearchParams(searchParams);
+    params.set("userId", userId);
+
+    router.push(pathname + "?" + params.toString());
+  };
+
+  useEffect(() => {
+    const dialogEl = document.getElementById(
+      props.dialogId,
+    ) as HTMLDialogElement | null;
+    if (!dialogEl) return;
+
+    dialogEl.addEventListener("close", onDialogClose);
+
+    return () => {
+      dialogEl.removeEventListener("close", onDialogClose);
+    };
+  }, [onDialogClose, props.dialogId]);
 
   return (
     <Button variant="gray" onClick={onClick}>

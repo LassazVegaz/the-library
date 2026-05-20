8c2ba5bf4823d76f301d6f9e64a11b3d730530e5
LassazVegaz <lasindunuwanga@gmail.com>
2026-05-20 15:17:07 +0800

    lend book page basic design

--- src/app/books/[id]/lend/page.tsx
+++ src/app/books/[id]/lend/page.tsx
@@ -1,3 +1,34 @@
+import { Button } from "@/components/FormComponents";
+import PageTitle from "@/components/PageTitle";
+import SubTitle from "@/components/SubTitle";
+
 export default function LendBookPage() {
-  return <div>Lend Book</div>;
+  return (
+    <div className="py-page-gutter grid grid-cols-2 grid-rows-[auto_1fr_auto] h-full gap-y-10">
+      <PageTitle className="col-[span_2]">Lend Book</PageTitle>
+
+      <div className="px-10 border-r border-amber-100">
+        <SubTitle>Book information</SubTitle>
+
+        <div className="mt-10">Title: Harry Porter 1</div>
+      </div>
+
+      <div className="px-10 grid grid-rows-[auto_1fr_auto]">
+        <SubTitle>User information</SubTitle>
+
+        <div className="mt-10">
+          <div>Name: John Done</div>
+          <div>Email: johndoen@john.com</div>
+        </div>
+
+        <div className="flex justify-center">
+          <Button variant="gray">Select user</Button>
+        </div>
+      </div>
+
+      <div className="col-[span_2] flex justify-center">
+        <Button variant="blue">Lend</Button>
+      </div>
+    </div>
+  );
 }

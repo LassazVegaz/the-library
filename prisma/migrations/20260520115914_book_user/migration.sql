210afabbc37657d263904741af8e5c5c813ccf1f
LassazVegaz <lasindunuwanga@gmail.com>
2026-05-20 20:00:09 +0800

    user book relationship

---
+++ prisma/migrations/20260520115914_book_user/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "_BookToUser" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_BookToUser_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE INDEX "_BookToUser_B_index" ON "_BookToUser"("B");
+
+-- AddForeignKey
+ALTER TABLE "_BookToUser" ADD CONSTRAINT "_BookToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Book"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_BookToUser" ADD CONSTRAINT "_BookToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
